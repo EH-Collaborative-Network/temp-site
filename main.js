ae40d9413cd8f9905eb57099ad4e6a3af8557189
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2022-03-18 13:27:45 -0400

    x for mobile nav

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-function showNav(){
+function showNav(el){
   if(document.querySelector("nav").classList.contains("open")){
     document.querySelector("nav").classList.remove("open")
+    el.classList.remove("open")
   }else{
     document.querySelector("nav").classList.add("open")
+    el.classList.add("open")
   }
 }
