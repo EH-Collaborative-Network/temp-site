6c37954486a30eeccbc45a7426ff1359478489e9
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2022-03-29 11:42:35 -0400

    semantic html and modal focus behavior on about and hp

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ function showNav(el){
   if(document.querySelector("nav").classList.contains("open")){
     document.querySelector("nav").classList.remove("open")
     el.classList.remove("open")
+    document.getElementById("mobile-menu-trigger").ariaExpanded = "false"
   }else{
     document.querySelector("nav").classList.add("open")
     el.classList.add("open")
+    document.getElementById("mobile-menu-trigger").ariaExpanded = "true"
   }
 }
